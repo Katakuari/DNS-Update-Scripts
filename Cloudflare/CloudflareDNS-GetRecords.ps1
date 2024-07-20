1de3ba17567b9a548f844047687fa054f37341a0
Katakuari <katakuarii@gmail.com>
2024-07-20 19:37:08 +0200

    Added a response filter for A  / AAAA

--- Cloudflare/CloudflareDNS-GetRecords.ps1
+++ Cloudflare/CloudflareDNS-GetRecords.ps1
@@ -1,4 +1,7 @@
-﻿$dnsconf = Get-Content "$PSScriptRoot\dnsconfig.json" | ConvertFrom-Json
+﻿# 19.07.2024
+# Script by Katakuari - dev@felesadastra.xyz
+
+$dnsconf = Get-Content "$PSScriptRoot\dnsconfig.json" | ConvertFrom-Json
 
 $headers = @{
 	"X-Auth-Email"  = "$($dnsconf.authEmail)"; 
@@ -7,5 +10,5 @@ $headers = @{
 }
 
 $response = Invoke-RestMethod "https://api.cloudflare.com/client/v4/zones/$($dnsconf.recordZoneId)/dns_records" -Method 'GET' -Headers $headers
-$response.result | Where-Object { $_.type -eq "A" } | Format-List id, name, type, content, comment
+$response.result | Where-Object { $_.type -eq "A" -or $_.type -eq "AAAA" } | Format-List id, name, type, content, comment
 
