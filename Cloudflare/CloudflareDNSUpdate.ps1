2ea01c51ae452ae2b25c2d7c1b94652419f38d22
Katakuari <katakuarii@gmail.com>
2024-07-20 22:37:01 +0200

    Fixed for loop, added file filter

--- Cloudflare/CloudflareDNSUpdate.ps1
+++ Cloudflare/CloudflareDNSUpdate.ps1
@@ -1,4 +1,8 @@
-﻿$dnsconf = Get-Content "$PSScriptRoot\dnsconfig.json" | ConvertFrom-Json
+﻿# 20.07.2024
+# Script by Katakuari - dev@felesadastra.xyz
+
+$dnsconfFile = Get-ChildItem -Path "$PSScriptRoot" -Filter "dnsconfig.json" -Recurse
+$dnsconf = Get-Content $dnsconfFile | ConvertFrom-Json
 
 $headers = @{
 	"X-Auth-Email"  = "$($dnsconf.authEmail)"; 
@@ -6,8 +10,8 @@ $headers = @{
 	"Content-Type"  = "application/json"
 }
 
-for ($i = 0; $i -ge $dnsconf.recordId.Length; $i++) {
-	
+for ($i = 0; $i -lt $dnsconf.recordId.Length; $i++) {
+	Write-Host $i
 	if ($dnsconf.recordType[$i] -eq "A") {
 		$recordValue = (curl.exe https://ifconfig.co/ip -4).Trim()
 	}
@@ -16,20 +20,19 @@ for ($i = 0; $i -ge $dnsconf.recordId.Length; $i++) {
 		$recordValue = (curl.exe https://ifconfig.co/ip -6).Trim()
 		if ($null -eq $recordValue) { break }
 	}
-	
-	
-	
+
 	$body = "{
 		""content"": ""$($recordValue)"",
 		""name"": ""$($dnsconf.recordName[$i])"",
 		""type"": ""$($dnsconf.recordType[$i])""
 		}"
 
+	Write-Host "Updating following entry:`n$body"
+	
 	$response = Invoke-RestMethod "https://api.cloudflare.com/client/v4/zones/$($dnsconf.recordZoneId)/dns_records/$($dnsconf.recordId[$i])" -Method 'PATCH' -Headers $headers -Body $body
 	$response | ConvertTo-Json
 }
 <#
 TODO:
 - Logging
-- If-loop for multiple entries
 #>
